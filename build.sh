4251c299d294b13945b9753c06d6a685e76aff0e
dentropy development <dentropy.development.vm@example.com>
2020-05-13 14:43:13 -0400

    Changing container names

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t dentropy/semantic-mediawiki .
+docker build -t semantic-mediawiki .
--- database-restore.sh
+++ database-restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Restore mariadb database
-docker exec -i mytry_database_1 sh -c 'exec mysql -uwikiuser -pexample' < ./semantic-mediawiki.sql
+docker exec -i semanticmediawikidocker_database_1 sh -c 'exec mysql -uwikiuser -pexample' < ./semantic-mediawiki.sql
 echo "Database Restore Complete"
